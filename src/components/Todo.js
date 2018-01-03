a77bacdaf7f221d795ff67eb2d1a826e99e9c5e7
Ryan Asensio <ryan@Ryans-MacBook-Air.local>
2018-01-02 21:10:17 -0600

    Fills in with tutorial Todo code. Pings rails app to retrieve records

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Todo = ({text}) => ( <li>{text}</li> )
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={ {
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+)
 
-export default Todo 
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+var server_todos = []
+window.fetch("http://0.0.0.0:3000/items").then(function(response) {
+	response.json().then(function(data) {
+//Set dummyDefaultStore with data
+		});
+})
+
+debugger;
+let dummyDefaultStore = {todos: server_todos}
+
+let store = createStore(todoApp, dummyDefaultStore)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
